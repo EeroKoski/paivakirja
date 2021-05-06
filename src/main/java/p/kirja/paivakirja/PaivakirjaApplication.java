99b08b0d99844d90b28cc66b74d9b3bc5ee235c0
EeroKoski <bfr563@myy.haaga-helia.fi>
2021-05-06 16:40:18 +0300

    suomi + kuvat

--- src/main/java/p/kirja/paivakirja/PaivakirjaApplication.java
+++ src/main/java/p/kirja/paivakirja/PaivakirjaApplication.java
@@ -29,22 +29,22 @@ public class PaivakirjaApplication {
 		return (args) -> {
 			log.info("esimerkkiviinit");
 
-				tyrepo.save(new Type("Red"));
-				tyrepo.save(new Type("White"));
-				tyrepo.save(new Type("Orange"));
-				tyrepo.save(new Type("Rosé"));
-				tyrepo.save(new Type("Fortified"));
-				tyrepo.save(new Type("Sparkling"));				
+				tyrepo.save(new Type("Punaviini"));
+				tyrepo.save(new Type("Valkoviini"));
+				tyrepo.save(new Type("Oranssiviini"));
+				tyrepo.save(new Type("Rosee"));
+				tyrepo.save(new Type("Väkevöity viini"));
+				tyrepo.save(new Type("Kuohuviini"));				
 				
 				
-				wirepo.save(new Wine("Alves de Sousa", "Abandonado", "2011", "DOC Douro", "Douro", "Portugal", "Touriga Nacional, Touriga Franca", tyrepo.findByName("Red").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
-				wirepo.save(new Wine("R. Lopez de Heredia", "Vina Tondonia", "1995", " DOCa Rioja Gran Reserva", "Rioja", "Spain", "Tempranillo", tyrepo.findByName("Red").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
-				wirepo.save(new Wine("Domaine Georges Vernay", "Terrasses de l'Empire", "2019", "AC Condrieu", "Rhone", "France", "Viognier", tyrepo.findByName("White").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
-				wirepo.save(new Wine("La Stoppa", "Ageno", "2011", "IGT Emilia-Romagna", "Emilia-Romagna", "Italy", "Malvasia", tyrepo.findByName("Orange").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
-				wirepo.save(new Wine("Domaine Schoffit", "Cuvee Catherine Lieu-dit Harth", "2000", "AC Alsace", "Alsace", "France", "Riesling", tyrepo.findByName("White").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
-				wirepo.save(new Wine("Bollinger", "La Grande Annee", "1988", "AC Champagne", "Champagne", "France", "Pinot Noir, Chardonnay, Pinot Meunier", tyrepo.findByName("Sparkling").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
-				wirepo.save(new Wine("Weingut Gessinger", "Zeltinger Sonnenuhr Caldo Infernale", "2010", "Prädikatswein Mosel Auslese", "Mosel-Saar-Ruwer", "Germany", "Riesling", tyrepo.findByName("White").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
-				wirepo.save(new Wine("Jurtschitsch", "Steinhaus", "2007", "DAC Kamptal", "Niederösterreich", "Austria", "Gruner Veltliner", tyrepo.findByName("White").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("Alves de Sousa", "Abandonado", "2011", "DOC Douro", "Douro", "Portugal", "Touriga Nacional, Touriga Franca", tyrepo.findByName("Punaviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("R. Lopez de Heredia", "Vina Tondonia", "1995", " DOCa Rioja Gran Reserva", "Rioja", "Spain", "Tempranillo", tyrepo.findByName("Punaviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("Domaine Georges Vernay", "Terrasses de l'Empire", "2019", "AC Condrieu", "Rhone", "France", "Viognier", tyrepo.findByName("Valkoviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("La Stoppa", "Ageno", "2011", "IGT Emilia-Romagna", "Emilia-Romagna", "Italy", "Malvasia", tyrepo.findByName("Oranssiviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("Domaine Schoffit", "Cuvee Catherine Lieu-dit Harth", "2000", "AC Alsace", "Alsace", "France", "Riesling", tyrepo.findByName("Valkoviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("Bollinger", "La Grande Annee", "1988", "AC Champagne", "Champagne", "France", "Pinot Noir, Chardonnay, Pinot Meunier", tyrepo.findByName("Kuohuviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("Weingut Gessinger", "Zeltinger Sonnenuhr Caldo Infernale", "2010", "Prädikatswein Mosel Auslese", "Mosel-Saar-Ruwer", "Germany", "Riesling", tyrepo.findByName("Valkoviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
+				wirepo.save(new Wine("Jurtschitsch", "Steinhaus", "2007", "DAC Kamptal", "Niederösterreich", "Austria", "Gruner Veltliner", tyrepo.findByName("Valkoviini").get(0), "Oikein kiva viini 95pistettä terveisin seppo", "95"));
 
 				
 				
